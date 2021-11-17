b7af577812b4c3d7a079cab94be751028d8f569c
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2021-11-17 01:48:55 -0300

    Ajustes detalhe do anúncio

--- src/screens/AnnouncementDetails/index.tsx
+++ src/screens/AnnouncementDetails/index.tsx
@@ -120,17 +120,17 @@ export function AnnouncementDetails(): ReactElement {
     return <TabBottom title="Chat" Icon={IconWechat} onPress={handleChat} />;
   }
 
-  function handleFavorite() {
-    if (favorite) {
-      UserApi.deleteAnnouncementFavorite(announcement.id).then(() => {
-        setFavorite(false);
-      });
-    } else {
-      UserApi.storeAnnouncementFavorite(announcement.id).then(() => {
-        setFavorite(true);
-      });
-    }
-  }
+  // function handleFavorite() {
+  //   if (favorite) {
+  //     UserApi.deleteAnnouncementFavorite(announcement.id).then(() => {
+  //       setFavorite(false);
+  //     });
+  //   } else {
+  //     UserApi.storeAnnouncementFavorite(announcement.id).then(() => {
+  //       setFavorite(true);
+  //     });
+  //   }
+  // }
 
   useEffect(() => {
     function getAnnouncementById() {
@@ -170,9 +170,18 @@ export function AnnouncementDetails(): ReactElement {
                   <IconLikeOrMore name="more-vert" size={24} />
                 </TouchableOpacity>
               ) : (
-                <TouchableOpacity onPress={() => handleFavorite()}>
+                <TouchableOpacity
+                  onPress={() => {
+                    setFavorite(!favorite);
+
+                    // eslint-disable-next-line no-unused-expressions
+                    favorite
+                      ? UserApi.deleteAnnouncementFavorite(announcement.id)
+                      : UserApi.storeAnnouncementFavorite(announcement.id);
+                  }}
+                >
                   <IconLikeOrMore
-                    name={favorite ? 'favorite' : 'favorite-border'}
+                    name={favorite ? 'favorite-border' : 'favorite'}
                     size={24}
                   />
                 </TouchableOpacity>
@@ -182,36 +191,35 @@ export function AnnouncementDetails(): ReactElement {
             </IconsContainer>
           </HeaderContent>
         </Header>
+        {showMoreOptions ? (
+          <MoreContent>
+            <Option
+              activeOpacity={0.7}
+              onPress={() => {
+                navigation.dispatch(
+                  CommonActions.navigate({
+                    name: 'EditAnnouncement',
+                    params: {
+                      ad: announcement,
+                    },
+                  }),
+                );
+
+                setShowMoreOptions(false);
+              }}
+            >
+              <TextOption>Editar</TextOption>
+            </Option>
+            <Option
+              activeOpacity={0.7}
+              // onPress={() => }
+            >
+              <TextOption>Excluir</TextOption>
+            </Option>
+          </MoreContent>
+        ) : null}
 
         <ScrollView showsVerticalScrollIndicator={false}>
-          {showMoreOptions ? (
-            <MoreContent>
-              <Option
-                activeOpacity={0.7}
-                onPress={() => {
-                  navigation.dispatch(
-                    CommonActions.navigate({
-                      name: 'EditAnnouncement',
-                      params: {
-                        ad: announcement,
-                      },
-                    }),
-                  );
-
-                  setShowMoreOptions(false);
-                }}
-              >
-                <TextOption>Editar</TextOption>
-              </Option>
-              <Option
-                activeOpacity={0.7}
-                // onPress={() => }
-              >
-                <TextOption>Excluir</TextOption>
-              </Option>
-            </MoreContent>
-          ) : null}
-
           <ImageSlider imagesUrl={announcement.images} />
 
           <AnnouncementContent>
